84d405e96eb4203406316cc2fdc106b157bb36e7
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-06-23 22:12:18 -0300

    Using useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
-const App = () => <Header />;
+const App = () => {
+    const [projects, setProjects] = useState(["Projeto 01", "Projeto 02"]);
+    const handleClick = () => setProjects(state => [...projects, `Projeto ${(state.length + 1).toString().padStart(2, '0')}`]);
+    return (<>
+        <Header />
+        <ul>
+            {projects.map(project => <li title={project}>{project}</li>)}
+        </ul>
+        <button onClick={handleClick}>Adicionar projeto</button>
+    </>)
+};
 
 export default App;
