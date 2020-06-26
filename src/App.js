d539aeee81443d82e35f7ec227cbb35664dc89f6
João Giovanni <joao.oliveira@maltacobrancas.com.br>
2020-06-25 21:38:42 -0300

    Loader for CSS and Image files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import './App.css'
+import backgroundImage from './assets/background.jpg';
 import Header from './components/Header';
 
 const App = () => {
     const [projects, setProjects] = useState(["Projeto 01", "Projeto 02"]);
     const handleClick = () => setProjects(state => [...projects, `Projeto ${(state.length + 1).toString().padStart(2, '0')}`]);
     return (<>
+        <img width={300} src={backgroundImage}/>
         <Header />
         <ul>
             {projects.map(project => <li title={project}>{project}</li>)}
